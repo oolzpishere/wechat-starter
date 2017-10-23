b0ad6ea3425baee4e7be0278ff3fc5887d693cee
leeli <leeli@leelis-iMac.local>
2017-10-23 14:05:45 +0800

    add invoices index logic

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,5 +1,18 @@
 module InvoicesHelper
+
+  def invoice_hash
+    {
+      title: '抬头',
+      tax_no: '税号',
+      addr: '地址',
+      phone: '联系方式',
+      bank_type: '银行类型',
+      bank_no: '银行号码',
+      create_time: '创建时间'
+    }
+  end
+
   def to_hash(json)
-    JSON.parse(json)
+    JSON.parse(json).deep_transform_keys {|k| k.underscore.to_sym}
   end
 end
