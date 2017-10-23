2a059a2dab5784316865f182b8fefd0fc0b71110
leeli <leeli@leelis-iMac.local>
2017-10-23 15:23:06 +0800

    add precompile

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -8,7 +8,8 @@ module InvoicesHelper
       phone: '联系方式',
       bank_type: '银行类型',
       bank_no: '银行号码',
-      create_time: '创建时间'
+      create_time: '创建时间',
+      updated_at: '更新时间'
     }
   end
 
